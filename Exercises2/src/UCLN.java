1b41007e1ee8c67997a50c9c821a28b09d63dfa5
quocTuan02 <thamphan1997@gmail.com>
2019-04-12 17:35:42 +0700

    javacore-b2

--- Exercises2/src/UCLN.java
+++ Exercises2/src/UCLN.java
@@ -18,7 +18,6 @@ public class UCLN {
 //            }
 //        }
 //        System.out.println(uc);
-
         for (int i=0; i<5;i++){
             for (int j=0; j<i; j++){
                 System.out.print(+j);
